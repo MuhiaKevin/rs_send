84c697b0283e9cfd24cec6c2df0746bc3787805c
MuhiaKevin <archerkreiger@gmail.com>
2024-06-12 16:42:50 +0300

    Cleanup

--- src/send_files.rs
+++ src/send_files.rs
@@ -6,8 +6,9 @@ use tokio::fs::File;
 use reqwest::{multipart, Client};
 use serde::{Deserialize, Serialize};
 use tokio_util::codec::{BytesCodec, FramedRead};
-// use sha256::try_digest;
 
+
+const HOST: &'static str = "http://192.168.2.101:53317";
 
 // Preupload response
 #[derive(Debug, Deserialize)]
@@ -16,43 +17,6 @@ struct Response {
     session_id: String,
     files: HashMap<String, String>,
 }
-
-
-const HOST: &'static str = "http://192.168.2.101:53317";
-
-// // for sending files after receiving the session id and file tokens
-// #[derive(Debug)]
-// struct SendFiles {
-//     session_id: String,
-//     files: Vec<FileSend>,
-// }
-//
-// impl SendFiles {
-//     fn build(response: Response) -> Self {
-//         let mut files_to_send: Vec<FileSend> = vec![];
-//
-//         for item in response.files {
-//             files_to_send.push(FileSend {
-//                 file_id: item.0,
-//                 file_token: item.1,
-//             });
-//         }
-//
-//         SendFiles {
-//             session_id: response.session_id,
-//             files: files_to_send,
-//         }
-//     }
-// }
-//
-// #[derive(Debug)]
-// struct FileSend {
-//     // file_name: String,
-//     // file_pointer: File,
-//     file_id: String,
-//     file_token: String,
-// }
-//
 
 
 // struct for initially starting the application
@@ -135,40 +99,7 @@ pub async fn send_files() {
     let pre_upload = PreUpload::build(&somethhing);
     let foo = serde_json::to_string(&pre_upload).unwrap();
 
-    // let url = "http://192.168.2.104:53317/api/localsend/v2/prepare-upload";
     let url = format!("{HOST}/api/localsend/v2/prepare-upload");
-    // let url = "http://localhost:53117/api/localsend/v2/prepare-upload";
-    
-    // let json_data = r#"{
-    //   "info": {
-    //     "alias": "Chifu wa Kizunu Files",
-    //     "version": "2.0", 
-    //     "deviceModel": "Burner phone", 
-    //     "deviceType": "mobile", 
-    //     "fingerprint": "random string", 
-    //     "port": 53317,
-    //     "protocol": "http",
-    //     "download": true 
-    //   },
-    //   "files": {
-    //     "this_is_id_0": {
-    //       "id": "this_is_id_0",
-    //       "fileName": "Readme.md",
-    //       "size": 260,
-    //       "fileType": "image/jpeg",
-    //       "sha256": "15dc2e55c8fe45170e732d6997d0ee1e4058d8c9d2df4a0b57f8e875e8ab994",
-    //       "preview": "*preview data*" 
-    //     },
-    //     "this_is_id_1": {
-    //       "id": "this_is_id_1",
-    //       "fileName": "Cargo.toml",
-    //       "size": 325,
-    //       "fileType": "image/jpeg",
-    //       "sha256": "7f2f888a48379a5579c053ee25b69c2beec2129f68f7067edc43ba9442400e1e",
-    //       "preview": "*preview data*" 
-    //     }
-    //   }
-    // }"#;
     
     let client = reqwest::Client::new();
 
@@ -182,31 +113,9 @@ pub async fn send_files() {
 
     let response_body: Response = response.json().await.unwrap();
     upload_files(response_body, somethhing).await;
-
-
-
-
-
-    // println!("Status Code: {}", response.status());
-    // let response_body = response.text().await.unwrap();
-    // let response_body: serde_json::Value = response.json().await.unwrap();
-
-
-    // for item in response_body.files.iter() {
-    //     println!("item id: {:?}, item token: {:?}", item.0, item.1);
-    // }
-
-    //   // get session id
-    //   let session_id = &response_body["sessionId"];
-    //   println!("sesionId {:?}", session_id.as_str());
-    //
-    // // get files
-    // let files = &response_body["files"];
-    // println!("files {:?}", files);
 }
 
  async fn upload_files(response_body: Response, files: Vec<OpenFiles>) {
-    // let url =  format!("http://192.168.2.107:53317/api/localsend/v2/upload?sessionId={}&fileId={}&token={}");
     let client = Client::new();
 
     for file in files {
@@ -251,9 +160,8 @@ pub async fn open_files_send() -> Vec<OpenFiles> {
 
         if file_path.exists() {
             let file_size = file_path.metadata().unwrap().size();
-            // let file_sha256 = try_digest(file_path).unwrap(); FIX: This is is slow for now
+            // let file_sha256 = sha256::try_digest(file_path).unwrap(); FIX: This is is slow for now
             let file_sha256 = String::from("Sha1asomsdashdjhjksad");
-            // let fp = File::open(file_path).unwrap();
             let fp = File::open(file_path).await.unwrap();
             let id = format!("this_is_id_{}", index);
             let real_file_name = file_path.file_name().unwrap().to_str().unwrap().to_string();
