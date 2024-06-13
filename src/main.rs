903a9ef20ff1ebd91caf01516a46b8aec9266d10
MuhiaKevin <archerkreiger@gmail.com>
2024-06-13 09:34:27 +0300

    Added anyhow for error handling; Some cargo fmt;

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ mod send_files;
 use send_files::send_files;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     // std::thread::spawn(|| discovery::get_discovered_by_clients());
     // //get_discovered_by_clients();
     // //discovery::udp_listen();
     // discovery::another_send().unwrap();
 
-    send_files().await
+    send_files().await?;
+    Ok(())
 }
--- src/send_files.rs
+++ src/send_files.rs
@@ -1,11 +1,10 @@
+use reqwest::{multipart, Client};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::os::unix::fs::MetadataExt;
 use std::path::Path;
 use tokio::fs::File;
-use reqwest::{multipart, Client};
-use serde::{Deserialize, Serialize};
 use tokio_util::codec::{BytesCodec, FramedRead};
-
 
 const HOST: &'static str = "http://192.168.2.107:53317";
 
@@ -16,7 +15,6 @@ struct Response {
     session_id: String,
     files: HashMap<String, String>,
 }
-
 
 // struct for initially starting the application
 #[derive(Debug, Serialize)]
@@ -41,15 +39,12 @@ pub struct OpenFiles {
     preview: String,
 }
 
-
-
 // For sending preupload
 #[derive(Debug, Serialize)]
- struct PreUpload<'a> {
+struct PreUpload<'a> {
     info: Settings,
     files: HashMap<String, &'a OpenFiles>,
 }
-
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Settings {
@@ -65,8 +60,7 @@ struct Settings {
     download: bool,
 }
 
-
-impl<'a> PreUpload<'a >  {
+impl<'a> PreUpload<'a> {
     fn build(opened_files: &'a Vec<OpenFiles>) -> Self {
         let mut foo_bar: HashMap<String, &OpenFiles> = HashMap::new();
 
@@ -76,83 +70,80 @@ impl<'a> PreUpload<'a >  {
 
         PreUpload {
             info: Settings {
-                alias:  "Chifu wa Kizunu Files".to_string(),
-                version:  "2.0".to_string(),
-                device_model:  "Burner Phone".to_string(),
-                device_type:  "mobile".to_string(),
-                fingerprint:  "random string".to_string(),
-                port:  53317,
-                protocol:  "http".to_string(),
+                alias: "Chifu wa Kizunu Files".to_string(),
+                version: "2.0".to_string(),
+                device_model: "Burner Phone".to_string(),
+                device_type: "mobile".to_string(),
+                fingerprint: "random string".to_string(),
+                port: 53317,
+                protocol: "http".to_string(),
                 download: true,
             },
-            files: foo_bar
-
+            files: foo_bar,
         }
     }
 }
 
+pub async fn send_files() -> anyhow::Result<()> {
+    let send_file_list = open_files_send().await;
 
-pub async fn send_files() {
-    let somethhing = open_files_send().await;
+    let pre_upload = PreUpload::build(&send_file_list);
 
-    let pre_upload = PreUpload::build(&somethhing);
-    let foo = serde_json::to_string(&pre_upload).unwrap();
+    let pre_upload_json = serde_json::to_string(&pre_upload)?;
 
-    let url = format!("{HOST}/api/localsend/v2/prepare-upload");
-    
+    let pre_upload_url = format!("{HOST}/api/localsend/v2/prepare-upload");
+
     let client = reqwest::Client::new();
 
     let response = client
-        .post(url)
+        .post(pre_upload_url)
         .header("Content-Type", "application/json")
-        .body(foo)
+        .body(pre_upload_json)
         .send()
-        .await
-        .unwrap();
+        .await?;
 
-    let response_body: Response = response.json().await.unwrap();
-    upload_files(response_body, somethhing).await;
+    let response_body: Response = response.json().await?;
+    let _ = upload_files(response_body, send_file_list).await;
+
+    Ok(())
 }
 
- async fn upload_files(response_body: Response, files: Vec<OpenFiles>) {
+async fn upload_files(response_body: Response, files: Vec<OpenFiles>) -> anyhow::Result<()> {
     let client = Client::new();
 
     for file in files {
         let token = response_body.files.get(&file.id).unwrap();
-        println!("token for file with id {:?} is {:?}", file.id, token);
-        let url =  format!("{HOST}/api/localsend/v2/upload?sessionId={}&fileId={}&token={}", response_body.session_id, file.id, token);
+        let url = format!(
+            "{HOST}/api/localsend/v2/upload?sessionId={}&fileId={}&token={}",
+            response_body.session_id, file.id, token
+        );
 
         // read file body stream
         let stream = FramedRead::new(file.file_pointer, BytesCodec::new());
         let file_body = reqwest::Body::wrap_stream(stream);
-        
+
         let part = multipart::Part::stream(file_body);
 
         let form = reqwest::multipart::Form::new()
             .text("resourceName", "filename.filetype")
             .part("FileData", part);
 
-
-        let res = client
-            .post(url)
-            .multipart(form)
-            .send().await.unwrap();
+        let res = client.post(url).multipart(form).send().await?;
 
         println!("{}", res.status());
-        println!("body: \n{}", res.text().await.unwrap());
+        println!("body: \n{}", res.text().await?);
     }
+
+    Ok(())
 }
 
 pub async fn open_files_send() -> Vec<OpenFiles> {
     let mut open_files: Vec<OpenFiles> = vec![];
-
     let files: Vec<String> = std::env::args().collect();
-    println!("{files:?}\n\n\n\n\n\n");
 
     if files.len() < 2 {
         eprintln!("Please Enter some files")
     }
-
 
     for (index, file_name) in files[1..].into_iter().enumerate() {
         let file_path = Path::new(&file_name);
