6d424ac79d89935f5d4ac7b5bf1c5298d9de7d93
MuhiaKevin <archerkreiger@gmail.com>
2024-06-17 11:59:23 +0300

    Send folder or file/files easily

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@
 mod send_files;
 mod send_folder;
 
-// use send_files::send_files;
+use std::path::Path;
+
+use send_files::send_files;
 use send_folder::send_folder;
 
 
@@ -17,9 +19,15 @@ async fn main() -> anyhow::Result<()> {
         std::process::exit(1);
     }
 
-    // send_files(file_args).await?;
-    send_folder(file_args[1].clone()).await?;
-    // send_files(file_args).await?;
+    let my_path = Path::new(&file_args[1]);
+
+    if my_path.is_dir() {
+        println!("This is a directory");
+        send_folder(file_args[1].clone()).await?;
+    } else {
+        println!("This is a file or files");
+        send_files(file_args).await?;
+    }
 
     Ok(())
 }
